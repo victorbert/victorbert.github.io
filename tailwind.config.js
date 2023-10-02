b1f28e2352b1148cce4c34a867b5d5379d856fcc
victorbert <victoriotan391@gmail.com>
2023-10-02 11:37:02 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./index1.html", "./index2.html", "./index3.html","./index4.html","./index6.html","./indexx.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
